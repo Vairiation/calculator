aa6a6406aa796d9157bf377cf151e826c26c8947
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-09-19 12:24:57 -0500

    Add event listeners to number buttons so that clicking concats the value to the input/readout

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function subtract(x, y) {
-    return x - y;
+  return x - y;
 }
 
 function multiply(x, y) {
-    return x * y;
+  return x * y;
 }
 
 function divide(x, y) {
-    return x / y;
+  return x / y;
 }
 
-let output;
-let lastOutput;
+let input = '';
+let history = '';
 let x;
 let y;
 
 function calculator() {
-    output = 0;
-
-    createEventListeners();
+  createEventListeners();
 }
 
 function createEventListeners() {
-    const inputClasses = [
-        percent,
-        one,
-        two,
-        three,
-        four,
-        five,
-        six,
-        seven,
-        eight,
-        nine,
-        zero,
-        decimal,
-    ];
-    const functionClasses = [
-        remove,
-        clear,
-        divide,
-        multiply,
-        minus,
-        plus,
-        negative,
-        equals,
-    ]
-    
-    for (let input of inputClasses) {
-        const btn = document.querySelector(`.${input}`);
-        const value = btn.textContent;
+  const inputClasses = [
+    'percent',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'zero',
+    'decimal',
+  ];
+  const functionClasses = [
+    'remove',
+    'clear',
+    'divide',
+    'multiply',
+    'minus',
+    'plus',
+    'negative',
+    'equals',
+  ]
 
-        btn.addEventListener('click', () => {
-            return output += value;
-        })
-        console.log(output);
-    }
+  for (let inputs of inputClasses) {
+    const btn = document.querySelector('.' + inputs);
+    const value = btn.textContent;
+    const readout = document.querySelector('.readout');
+
+    btn.addEventListener('click', () => {
+      input += value;
+      readout.innerText = input;
+      console.log(input);
+      // return output += value;
+    })
+    console.log(input);
+  }
 }
+createEventListeners();
 console.log(add(1, 3), subtract(1, 4), multiply(3.5, 3), divide(27, 2));
